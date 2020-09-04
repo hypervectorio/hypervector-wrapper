a0e2aa8b18bbe307fff01f3ea0586f1522f5e443
Jason Costello <jjacostello@gmail.com>
2020-09-04 22:32:44 +0100

    Support for definition endpoint + simple test case

--- hypervector/resources/core/definition.py
+++ hypervector/resources/core/definition.py
@@ -1,3 +1,8 @@
+import json
+
+import requests
+
+import hypervector
 from hypervector.resources.core.ensemble import Ensemble
 from hypervector.resources.abstract.api_resource import APIResource
 
@@ -5,24 +10,35 @@ from hypervector.resources.abstract.api_resource import APIResource
 class Definition(APIResource):
     resource_name = 'definition'
 
-    def __init__(self, definition_uuid, definition_name, ensembles=None):
+    def __init__(self, definition_uuid, definition_name, added):
         self.definition_uuid = definition_uuid
         self.definition_name = definition_name
-        self.ensembles = ensembles
+        self.added = added
 
     @classmethod
     def from_dict(cls, definition_uuid, dictionary):
         return cls(
             definition_uuid=definition_uuid,
-            definition_name=dictionary['definition_name'],
-            ensembles=_parse_ensembles(dictionary['ensembles'])
+            definition_name=dictionary['definition_name']
         )
 
+    @classmethod
+    def from_response(cls, dictionary):
+        return cls(
+            definition_uuid=dictionary['definition_uuid'],
+            definition_name=dictionary['definition_name'],
+            added=dictionary['added']
+        )
 
-def _parse_ensembles(ensembles):
-    parsed_ensembles = []
-    for ensemble_uuid, ensemble_meta in ensembles.items():
-        parsed_ensemble = Ensemble.from_dict(ensemble_uuid, ensemble_meta)
-        parsed_ensembles.append(parsed_ensemble)
-    return parsed_ensembles
+    @classmethod
+    def new(cls, definition_file, project_uuid=None):
+        endpoint = f"{hypervector.API_BASE}/{cls.resource_name}"
+        data = _parse_definition_from_json_file(definition_file)
+        data['project_uuid'] = project_uuid
+        response = requests.post(endpoint, json=data, headers=cls.get_headers()).json()
+        return cls.from_response(response)
+
+
+def _parse_definition_from_json_file(definition_json_file):
+    return json.load(open(definition_json_file, 'rb'))
 
--- tests/test_project_api_resource.py
+++ tests/test_project_api_resource.py
@@ -1,7 +1,13 @@
 import hypervector
+from tests.util import get_resource_path
 
 
 def test_project_creation():
     new_project = hypervector.Project.new()
 
     assert isinstance(new_project, hypervector.Project)
+
+    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
+                                                definition_file=get_resource_path("hyperdef.json"))
+
+    assert isinstance(new_definition, hypervector.Definition)
