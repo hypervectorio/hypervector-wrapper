a6443232bb1401f1131bccd3fa05948cc1dd1a3d
Jason Costello <jjacostello@gmail.com>
2020-09-09 23:31:04 +0100

    Addition of Benchmark core API resource

--- hypervector/resources/core/__init__.py
+++ hypervector/resources/core/__init__.py
@@ -1,3 +1,4 @@
 from hypervector.resources.core.project import Project
 from hypervector.resources.core.ensemble import Ensemble
 from hypervector.resources.core.definition import Definition
+from hypervector.resources.core.benchmark import Benchmark
---
+++ hypervector/resources/core/benchmark.py
@@ -0,0 +1,14 @@
+from hypervector.resources.abstract.api_resource import APIResource
+
+
+class Benchmark(APIResource):
+    resource_name = 'benchmark'
+
+    def __init__(self, benchmark_uuid):
+        self.benchmark_uuid = benchmark_uuid
+
+    @classmethod
+    def from_dict_for_lists(cls, dictionary):
+        return cls(
+            benchmark_uuid=dictionary['benchmark_uuid']
+        )
