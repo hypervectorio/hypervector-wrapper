4d900b76a6a9f158326f1528fa75e966eae120ef
Jason Costello <jason@hypervector.io>
2020-10-09 10:07:06 +0100

    [WIP] Addition of .compare() method for Benchmark API resource

--- hypervector/resources/core/benchmark.py
+++ hypervector/resources/core/benchmark.py
@@ -17,8 +17,14 @@ class Benchmark(APIResource):
         )
 
     @classmethod
-    def new(cls, ensemble_uuid, output_hash):
+    def new(cls, ensemble_uuid, output):
         endpoint = f"{hypervector.API_BASE}/{cls.resource_name}"
-        data = {'ensemble_uuid': ensemble_uuid, "output_hash": output_hash}
+        data = {'ensemble_uuid': ensemble_uuid, "output_hash": hash(tuple(output))}
         response = requests.post(endpoint, json=data, headers=cls.get_headers()).json()
         return cls.from_response(response)
+
+    def compare(self, output):
+        endpoint = f"{hypervector.API_BASE}/{self.resource_name}"
+        data = {'benchmark_uuid': self.benchmark_uuid, "output_hash": hash(tuple(output))}
+        response = requests.get(endpoint, json=data, headers=self.get_headers()).json()
+        return response
