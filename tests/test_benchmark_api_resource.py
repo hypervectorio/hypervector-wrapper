9ecb972d243a29a7efd87b498e5d1116fb4a84d6
Jason Costello <jason@hypervector.io>
2020-10-09 10:07:27 +0100

    Minimum viable test for Benchmark compare() operation

--- tests/test_benchmark_api_resource.py
+++ tests/test_benchmark_api_resource.py
@@ -31,3 +31,26 @@ def test_benchmark_new():
     )
 
     assert isinstance(benchmark, hypervector.Benchmark)
+
+
+def test_benchmark_assert():
+
+    n_hypervectors = 10000
+
+    new_project = hypervector.Project.new()
+    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
+                                                definition_file=get_resource_path("hyperdef.json"))
+    new_ensemble = hypervector.Ensemble.new(
+        new_definition.definition_uuid,
+        n_hypervectors
+    )
+
+    benchmark_results = [random() for _ in range(n_hypervectors)]
+
+    benchmark = hypervector.Benchmark.new(
+        new_ensemble.ensemble_uuid,
+        benchmark_results
+    )
+
+    assert benchmark.compare(benchmark_results) == {'result': 'PASSED'}
+
