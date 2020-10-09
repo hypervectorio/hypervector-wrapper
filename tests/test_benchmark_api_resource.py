e86c3bd6729301718c0f9317d4cc14687ec357c7
Jason Costello <jason@hypervector.io>
2020-10-09 20:48:02 +0100

    Adding fixture to tidy test module

--- tests/test_benchmark_api_resource.py
+++ tests/test_benchmark_api_resource.py
@@ -1,7 +1,21 @@
 from random import random
-
+import pytest
 import hypervector
 from tests.util import get_resource_path
+
+
+@pytest.fixture
+def ensemble_for_test():
+    n_hypervectors = 10000
+    new_project = hypervector.Project.new()
+    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
+                                                definition_file=get_resource_path("hyperdef.json"))
+    new_ensemble = hypervector.Ensemble.new(
+        new_definition.definition_uuid,
+        n_hypervectors
+    )
+
+    return new_ensemble, n_hypervectors
 
 
 def test_benchmark_list():
@@ -11,24 +25,8 @@ def test_benchmark_list():
         assert isinstance(benchmark, hypervector.Benchmark)
 
 
-def test_benchmark_get():
-    benchmarks = hypervector.Benchmark.list()
-
-    for benchmark in benchmarks:
-        assert isinstance(benchmark, hypervector.Benchmark)
-
-
-def test_benchmark_new():
-
-    n_hypervectors = 10000
-
-    new_project = hypervector.Project.new()
-    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
-                                                definition_file=get_resource_path("hyperdef.json"))
-    new_ensemble = hypervector.Ensemble.new(
-        new_definition.definition_uuid,
-        n_hypervectors
-    )
+def test_benchmark_new(ensemble_for_test):
+    new_ensemble, n_hypervectors = ensemble_for_test
 
     benchmark_results = [random() for _ in range(n_hypervectors)]
 
@@ -40,17 +38,8 @@ def test_benchmark_new():
     assert isinstance(benchmark, hypervector.Benchmark)
 
 
-def test_benchmark_assert():
-
-    n_hypervectors = 10000
-
-    new_project = hypervector.Project.new()
-    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
-                                                definition_file=get_resource_path("hyperdef.json"))
-    new_ensemble = hypervector.Ensemble.new(
-        new_definition.definition_uuid,
-        n_hypervectors
-    )
+def test_benchmark_assert(ensemble_for_test):
+    new_ensemble, n_hypervectors = ensemble_for_test
 
     benchmark_results = [random() for _ in range(n_hypervectors)]
     failing_results = [random() for _ in range(n_hypervectors)]
