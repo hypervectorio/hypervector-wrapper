9939af00514eccb9c8612b4e5e4ae265a75f2e98
Jason Costello <jason@hypervector.io>
2021-02-18 20:26:16 +0000

    Tests updated to follow new benchmark and ensemble endpoints

--- tests/test_benchmark_api_resource.py
+++ tests/test_benchmark_api_resource.py
@@ -6,50 +6,47 @@ from tests.util import get_resource_path
 
 @pytest.fixture
 def ensemble_for_test():
-    n_hypervectors = 10000
     new_project = hypervector.Project.new()
     new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
                                                 definition_file=get_resource_path("hyperdef.json"))
     new_ensemble = hypervector.Ensemble.new(
-        new_definition.definition_uuid,
-        n_hypervectors
+        definition_uuid=new_definition.definition_uuid,
+        size='MEDIUM'
     )
 
-    return new_ensemble, n_hypervectors
+    return new_ensemble
 
 
-def test_benchmark_list():
-    benchmarks = hypervector.Benchmark.list()
+def test_benchmark_list(ensemble_for_test):
+    benchmarks = hypervector.Benchmark.list(ensemble_for_test.ensemble_uuid)
 
     for benchmark in benchmarks:
         assert isinstance(benchmark, hypervector.Benchmark)
 
 
 def test_benchmark_new(ensemble_for_test):
-    new_ensemble, n_hypervectors = ensemble_for_test
 
-    benchmark_results = [random() for _ in range(n_hypervectors)]
+    benchmark_results = [random() for _ in range(ensemble_for_test.size)]
 
     benchmark = hypervector.Benchmark.new(
-        new_ensemble.ensemble_uuid,
-        benchmark_results
+        ensemble_uuid=ensemble_for_test.ensemble_uuid,
+        output=benchmark_results
     )
 
     assert isinstance(benchmark, hypervector.Benchmark)
 
 
 def test_benchmark_assert(ensemble_for_test):
-    new_ensemble, n_hypervectors = ensemble_for_test
 
-    benchmark_results = [random() for _ in range(n_hypervectors)]
-    failing_results = [random() for _ in range(n_hypervectors)]
+    benchmark_results = [random() for _ in range(ensemble_for_test.size)]
+    failing_results = [random() for _ in range(ensemble_for_test.size)]
 
     benchmark = hypervector.Benchmark.new(
-        new_ensemble.ensemble_uuid,
-        benchmark_results
+        ensemble_uuid=ensemble_for_test.ensemble_uuid,
+        output=benchmark_results
     )
 
-    assert benchmark.compare(benchmark_results) == {'result': 'PASSED'}
-    assert benchmark.compare(failing_results) == {'result': 'FAILED'}
+    assert benchmark.assert_equal(benchmark_results)['asserted'] is True
+    assert benchmark.assert_equal(failing_results)['asserted'] is False
     
 
