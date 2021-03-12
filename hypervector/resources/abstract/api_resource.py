8fd8dac1595fb7ca362db7cc313eaf9a3bf42b64
Jason Costello <jason@hypervector.io>
2021-03-12 20:11:23 +0000

    Adding .delete() classmethod for resources, plus basic checking for success on get

--- hypervector/resources/abstract/api_resource.py
+++ hypervector/resources/abstract/api_resource.py
@@ -18,6 +18,15 @@ class APIResource:
     @classmethod
     def get(cls, uuid):
         endpoint = f'{hypervector.API_BASE}/{cls.resource_name}/{uuid}'
-        response = requests.get(endpoint, headers=cls.get_headers()).json()
-        return cls.from_response_get(response)
+        response = requests.get(endpoint, headers=cls.get_headers())
+        if response.status_code == 200:
+            return cls.from_response_get(response.json())
+        else:
+            return None
+
+    @classmethod
+    def delete(cls, uuid):
+        endpoint = f'{hypervector.API_BASE}/{cls.resource_name}/{uuid}/delete'
+        response = requests.delete(endpoint, headers=cls.get_headers()).json()
+        return response
 
--- tests/test_api_resource.py
+++ tests/test_api_resource.py
@@ -1,4 +1,16 @@
+import pytest
 import hypervector
+from tests.util import get_resource_path
+
+
+@pytest.fixture
+def test_definition():
+    project = hypervector.Project.new()
+    definition = hypervector.Definition.new(
+        definition_file=get_resource_path("hyperdef.json"),
+        project_uuid=project.project_uuid
+    )
+    return definition
 
 
 def test_resources():
@@ -13,6 +25,15 @@ def test_resources():
             for definition_meta in project.definitions:
                 definition = hypervector.Definition.get(definition_meta.definition_uuid)
                 assert isinstance(definition, hypervector.Definition)
+
+
+def test_delete_resource(test_definition):
+    hypervector.Definition.delete(test_definition.definition_uuid)
+
+    x = hypervector.Definition.get(test_definition.definition_uuid)
+
+    assert 1 == 1
+
 
 
 
