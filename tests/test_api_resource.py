b080c142a28a6accd89c284fca537986a8829168
Jason Costello <jason@hypervector.io>
2021-03-13 08:00:37 +0000

    Explicitly testing each resource delete()

--- tests/test_api_resource.py
+++ tests/test_api_resource.py
@@ -13,7 +13,25 @@ def test_definition():
     return definition
 
 
-def test_resources():
+@pytest.fixture
+def test_ensemble(test_definition):
+    ensemble = hypervector.Ensemble.new(
+        definition_uuid=test_definition.definition_uuid,
+        size=100
+    )
+    return ensemble
+
+
+@pytest.fixture
+def test_benchmark(test_ensemble):
+    benchmark = hypervector.Benchmark.new(
+        ensemble=test_ensemble,
+        expected_output=[1 for _ in range(test_ensemble.size)]
+    )
+    return benchmark
+
+
+def test_get_resource():
     projects = hypervector.Project.list()
 
     assert len(projects) > 0
@@ -27,7 +45,13 @@ def test_resources():
                 assert isinstance(definition, hypervector.Definition)
 
 
-def test_delete_resource(test_definition):
+def test_delete_resource(test_definition, test_ensemble, test_benchmark):
+    hypervector.Benchmark.delete(test_benchmark.benchmark_uuid)
+    assert hypervector.Benchmark.get(test_benchmark.benchmark_uuid) is None
+
+    hypervector.Ensemble.delete(test_ensemble.ensemble_uuid)
+    assert hypervector.Ensemble.get(test_ensemble.ensemble_uuid) is None
+
     hypervector.Definition.delete(test_definition.definition_uuid)
     assert hypervector.Definition.get(test_definition.definition_uuid) is None
 
