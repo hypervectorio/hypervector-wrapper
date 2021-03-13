ac2fb4a59b1b4f1467591cb33cb32bffdbead944
Jason Costello <jason@hypervector.io>
2021-03-13 19:42:30 +0000

    [WIP] Adding basic wrapper Definition tests

--- tests/test_definition_api_resource.py
+++ tests/test_definition_api_resource.py
@@ -1,4 +1,13 @@
+import pytest
 import hypervector
+from tests.util import get_resource_path
+from hypervector.resources.core.definition import _parse_definition_from_json_file
+
+
+@pytest.fixture
+def test_project():
+    project = hypervector.Project.new()
+    return project
 
 
 def test_definition_list():
@@ -6,3 +15,14 @@ def test_definition_list():
 
     for definition in definitions:
         assert isinstance(definition, hypervector.Definition)
+
+
+def test_definition_new(test_project):
+    definition = hypervector.Definition.new(
+        definition_file=get_resource_path("hyperdef.json"),
+        project_uuid=test_project.project_uuid
+    )
+
+    definition_loaded_directly = _parse_definition_from_json_file(get_resource_path("hyperdef.json"))
+
+    assert definition_loaded_directly['definition_name'] == definition.definition_name
