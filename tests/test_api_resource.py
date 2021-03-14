6f3997f0194eb95c236aa5392bf62ea4feb8c105
Jason Costello <jason@hypervector.io>
2021-03-14 17:01:44 +0000

    [WIP] Mocking further core methods for resources

--- tests/test_api_resource.py
+++ tests/test_api_resource.py
@@ -1,5 +1,6 @@
 import uuid
 import pytest
+import requests
 import responses
 
 import hypervector
@@ -58,13 +59,33 @@ def test_get_resource(definition, ensemble, benchmark):
     assert retrieved_definition.definition_uuid == definition.definition_uuid
 
 
+@responses.activate
 def test_get_resource_not_found():
+    random_resource_uuid = str(uuid.uuid4())
+
+    responses.add(
+        responses.GET,
+        f'{hypervector.API_BASE}/definition/{random_resource_uuid}',
+        status=404
+    )
+
     with pytest.raises(HypervectorError) as error:
-        hypervector.Definition.get(str(uuid.uuid4()))
+        hypervector.Definition.get(random_resource_uuid)
         assert error.status_code == 404
 
 
+@responses.activate
 def test_delete_resource(definition, ensemble, benchmark):
+    endpoints = [
+        f'{hypervector.API_BASE}/definition/{definition.definition_uuid}',
+        f'{hypervector.API_BASE}/ensemble/{ensemble.ensemble_uuid}',
+        f'{hypervector.API_BASE}/benchmark/{benchmark.benchmark_uuid}',
+    ]
+
+    for endpoint in endpoints:
+        responses.add(responses.DELETE, endpoint + "/delete", "", status=200)
+        responses.add(responses.GET, endpoint, status=404)
+
     hypervector.Benchmark.delete(benchmark.benchmark_uuid)
     with pytest.raises(HypervectorError) as error:
         hypervector.Benchmark.get(benchmark.benchmark_uuid)
