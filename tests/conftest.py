d9417af87095940d60ee9dcdceff0d1386c6b60e
Jason Costello <jason@hypervector.io>
2021-03-15 17:06:39 +0000

    [WIP] Fixtures for responses-based mocking moved to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,18 @@
+import pytest
+import responses
+
 import hypervector
+from tests import util
 
 hypervector.API_KEY = "dummy_value_not_a_valid_api_key"
+
+
+@pytest.fixture
+def mocked_responses():
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as rsps:
+        yield rsps
+
+
+@pytest.fixture
+def mocked_resources(mocked_responses):
+    return util.mocked_resources(mocked_responses)
