333f23ea75a6d6a42f5d1cf88e1bb6fb804e1e45
Jason Costello <jason@hypervector.io>
2021-03-15 22:36:32 +0000

    Benchmark mocked - all pre-existing tests now mocked

--- tests/test_benchmark_api_resource.py
+++ tests/test_benchmark_api_resource.py
@@ -1,47 +1,72 @@
+import uuid
 from random import random
-import pytest
+import responses
+
 import hypervector
-from tests.util import get_resource_path
 
 
-@pytest.fixture
-def ensemble_for_test():
-    new_project = hypervector.Project.new()
-    new_definition = hypervector.Definition.new(project_uuid=new_project.project_uuid,
-                                                definition_file=get_resource_path("hyperdef.json"))
-    new_ensemble = hypervector.Ensemble.new(
-        definition_uuid=new_definition.definition_uuid,
-        size='MEDIUM'
+def test_benchmark_list(mocked_resources, mocked_responses):
+    _, definition, ensemble, benchmark = mocked_resources
+
+    mocked_responses.add(
+        responses.GET,
+        f'{hypervector.API_BASE}/definition/{definition.definition_uuid}/'
+        f'ensemble/{ensemble.ensemble_uuid}/benchmarks',
+        json=[
+            benchmark.to_response(),
+            benchmark.to_response(),
+            benchmark.to_response()
+        ]
     )
 
-    return new_ensemble
+    retrieved_benchmarks = hypervector.Benchmark.list(ensemble)
+
+    for retrieved_benchmark in retrieved_benchmarks:
+        assert isinstance(retrieved_benchmark, hypervector.Benchmark)
 
 
-def test_benchmark_list(ensemble_for_test):
-    benchmarks = hypervector.Benchmark.list(ensemble_for_test)
-
-    for benchmark in benchmarks:
-        assert isinstance(benchmark, hypervector.Benchmark)
-
-
-def test_benchmark_new(ensemble_for_test):
-    benchmark_results = [random() for _ in range(ensemble_for_test.size)]
+def test_benchmark_new(mocked_resources):
+    _, definition, ensemble, _ = mocked_resources
+    benchmark_results = [random() for _ in range(ensemble.size)]
 
     benchmark = hypervector.Benchmark.new(
-        ensemble=ensemble_for_test,
+        ensemble=ensemble,
         expected_output=benchmark_results
     )
 
     assert isinstance(benchmark, hypervector.Benchmark)
 
 
-def test_benchmark_assert(ensemble_for_test):
-    benchmark_results = [random() for _ in range(ensemble_for_test.size)]
-    failing_results = [random() for _ in range(ensemble_for_test.size)]
+def test_benchmark_assert(mocked_resources, mocked_responses):
+    _, _, ensemble, benchmark = mocked_resources
 
-    benchmark = hypervector.Benchmark.new(
-        ensemble=ensemble_for_test,
-        expected_output=benchmark_results
+    benchmark_results = [random() for _ in range(ensemble.size)]
+    failing_results = [random() for _ in range(ensemble.size)]
+
+    mocked_responses.add(
+        responses.POST,
+        f'{hypervector.API_BASE}/benchmark/{benchmark.benchmark_uuid}/assert',
+        json={
+            "assertion_uuid": str(uuid.uuid4()),
+            "timestamp": "Mon, 1 Jan 1970 00:00:05 GMT",
+            "benchmark_uuid": benchmark.benchmark_uuid,
+            "ensemble_uuid": ensemble.ensemble_uuid,
+            "asserted": True,
+            "diff": None
+        }
+    )
+
+    mocked_responses.add(
+        responses.POST,
+        f'{hypervector.API_BASE}/benchmark/{benchmark.benchmark_uuid}/assert',
+        json={
+            "assertion_uuid": str(uuid.uuid4()),
+            "timestamp": "Mon, 1 Jan 1970 00:00:05 GMT",
+            "benchmark_uuid": benchmark.benchmark_uuid,
+            "ensemble_uuid": ensemble.ensemble_uuid,
+            "asserted": False,
+            "diff": None
+        }
     )
 
     assert benchmark.assert_equal(benchmark_results)['asserted'] is True
