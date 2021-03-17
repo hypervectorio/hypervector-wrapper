61488d1fbe0851e515f284092b83efde95cdf310
Jason Costello <jason@hypervector.io>
2021-03-17 12:11:07 +0000

    Fixing PyPI classifiers

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
