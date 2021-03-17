181f40ee0f57aeeb39b69a1681795124c9f4b2b3
Jason Costello <jason@hypervector.io>
2021-03-17 11:39:12 +0000

    Adding more details to setup.py and README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # hypervector-wrapper
 
-Python wrapper to access the Hypervector API
+Python wrapper to access the Hypervector API, providing easy deserialisation of Hypervector
+resources to Python objects for building your data-driven test suite.
+
+## Docs
+
+Full API and wrapper docs are available at [https://docs.hypervector.io](https://docs.hypervector.io), along with
+release history and usage examples.
+
+## Installation
+
+```python
+pip install hypervector-wrapper
+```
+
+Note: requires Python 3.6+
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hypervector',
+    name='hypervector-wrapper',
     version='0.0.1',
     description='Python wrapper to use the Hypervector API',
-    author='J. J. A. Costello',
-    packages=find_packages()
+    author='Jason Costello, Hypervector Limited',
+    author_email="jason@hypervector.io",
+    url="https://github.com/hypervectorio/hypervector-wrapper",
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
