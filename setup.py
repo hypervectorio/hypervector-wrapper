f43698093290f7d3d562a16581287521d60d46d9
Jason Costello <jason@hypervector.io>
2021-03-17 11:54:21 +0000

    [WIP] Adding README as long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='hypervector-wrapper',
     version='0.0.1',
     description='Python wrapper to use the Hypervector API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jason Costello, Hypervector Limited',
     author_email="jason@hypervector.io",
     url="https://github.com/hypervectorio/hypervector-wrapper",
