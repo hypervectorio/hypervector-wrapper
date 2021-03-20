f29932b08a14ee76673d22294053c2bcf3683f4e
Jason Costello <jason@hypervector.io>
2021-03-20 15:50:19 +0000

    Version bump to 0.03 (Ensemble GET compression support)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hypervector-wrapper',
-    version='0.0.2',
+    version='0.0.3',
     description='Python wrapper to use the Hypervector API',
     long_description=long_description,
     long_description_content_type="text/markdown",
