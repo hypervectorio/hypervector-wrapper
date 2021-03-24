0ad06359b71774c3fda88eb89cfa2ddfa580c982
Jason Costello <jason@hypervector.io>
2021-03-24 01:45:16 +0000

    Uprev to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hypervector-wrapper',
-    version='0.0.5',
+    version='0.0.6',
     description='Python wrapper to use the Hypervector API',
     long_description=long_description,
     long_description_content_type="text/markdown",
